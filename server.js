fcee74d870482182719d0e28ddaf32cbc1b517c5
Jie Feng <feng.jie1@northeastern.edu>
2022-04-07 16:56:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ import mongoose from "mongoose";
 
 const mongoAtLasPass = process.env.MONGO_AT_LAS_PASS;
 mongoose.connect(
-  //`mongodb://localhost:27017/webdev` ||
-  `mongodb+srv://admin:admin@cluster0.urank.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` 
+  `mongodb://localhost:27017/webdev` ||
+  `mongodb+srv://admin:${mongoAtLasPass}@cluster0.urank.mongodb.net/myFirstDatabase?retryWrites=true&w=majority` 
 );
 
 const app = express();
